a6066c9971b01fb8c792d8b46597e0265eb3b70a
Theflash39 <160582837+Theflash39@users.noreply.github.com>
2024-05-21 22:56:47 +0530

    Add files via upload
    added index.html

---
+++ texttosp.js
@@ -0,0 +1,23 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voicesSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length > 0) {
+        voices.forEach((voice, i) => {
+            voicesSelect.options[i] = new Option(voice.name, i);
+        });
+        speech.voice = voices[0]; // Assigning the first voice to the speech object
+        voices.forEach((voices,i) => (voicesSelect.options[i]= new Option(voice.name,i)));
+    }
+
+};
+voicesSelect.addEventListener("change",() => {
+    speech.voice=voices[voicesSelect.value]
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
